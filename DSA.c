571ad576ba34188bdb599bb237d2ba8326f5d285
Aditya Kumar <aryan68125@gmail.com>
2020-10-24 22:33:45 +0530

    Add files via upload

--- DSA.c
+++ DSA.c
@@ -22,6 +22,9 @@ void maxArray();
 void BinarySearchArray();
 void LinearSearchArray();
 void deleteArray();
+void MergeArray();
+void displayArrayTwo();
+void CreateArrayTwo();
 
 int *arr;
 int size;
@@ -29,6 +32,11 @@ int length;
 
 int *brr;
 int lengthb;
+int sizeb;
+
+int *crr;
+int lengthc=0;
+int sizec;
 
 void createArray() //containing the logic for creaton of array with ability to enter the size dynamically during runtime
 {
@@ -462,6 +470,128 @@ void rotateArray() //this function will rotate the elements inside the array
    arr[length-1]=tempo; //now copying the element that used to be at the 0th positon in the array to the last position in the array
    printf("Rotation of array elements SUCESSFUL!!!!!.....\n");
 
+}
+
+//from here on out functions of binary operations on array starts
+
+void CreateArrayTwo() //this function will merge the elements of arrayA and arrayB in ascending order in ArrayC
+{
+  //creation of Array A
+ printf("Enter the size of Array A\n");
+ scanf("%d",&size);
+ printf("Enter the number of elements you want to insert in Array A\n");
+ scanf("%d",&length);
+ arr=(int*)malloc(size*sizeof(int));  //Array A
+ if (length<=size)
+ {
+  printf("Enter the elements inside Array A\n");
+  for(int i=0;i<length;i++)
+ {
+  scanf("%d",&arr[i]);
+ }
+
+  int freeA = size - length;
+  printf("Free space in Array A is %d\n",freeA);
+ }
+  if(length>size)
+  {
+    printf("Array A creation Failed!!!!\n");
+    printf("Number of elements you want to enter is more than the size of Array A\n");
+    printf("Array A size =%d\n", size);
+    printf("number of elements to be entered inside Array A = %d\n",length);
+  }
+
+  //creation of Array B
+ printf("Enter the size of Array B\n");
+ scanf("%d",&sizeb);
+ printf("Enter the number of elements you want to insert in Array B\n");
+ scanf("%d",&lengthb);
+ brr=(int*)malloc(sizeb*sizeof(int));  //Array B
+ if (lengthb<=sizeb)
+ {
+  printf("Enter the elements inside Array B\n");
+  for(int i=0;i<lengthb;i++)
+ {
+  scanf("%d",&brr[i]);
+ }
+
+  int freeB = sizeb - lengthb;
+  printf("Free space in Array B is %d\n",freeB);
+ }
+  if(lengthb>sizeb)
+  {
+    printf("Array B creation Failed!!!!\n");
+    printf("Number of elements you want to enter is more than the size of Array B\n");
+    printf("Array B size =%d\n", sizeb);
+    printf("number of elements to be entered inside Array B = %d\n",lengthb);
+  }
+
+  //creation of Array C
+ crr=(int*)malloc(sizec*sizeof(int));  //Array C
+ sizec = size + sizeb;
+ lengthc=0;
+
+}
+
+void displayArrayTwo()
+{
+  printf("Printing Array A\n");
+  for(int i=0;i<length;i++)
+  {
+    printf("A[%d] = %d\n",i,arr[i]);
+  }
+  printf("Printing Array B\n");
+  for(int j=0;j<lengthb;j++)
+  {
+    printf("B[%d] = %d\n",j,brr[j]);
+  }
+  if(lengthc == 0)
+  {
+    printf("Array C is empty\n");
+  }
+  else
+  {
+    printf("Printing Array C\n");
+    for(int k=0;k<lengthc;k++)
+  {
+    printf("C[%d] = %d\n",k,crr[k]);
+  }
+  }
+}
+
+void MergeArray()
+{
+   int i; //for Traversing array A
+   int j; //for traversing Array B
+   int k=0; //for traversing Array C
+   while(i<length && j<lengthb)
+   {
+    if(arr[i]<brr[j])
+    {
+      crr[k]=arr[i];
+      k++;
+      i++;
+    }
+    else
+    {
+      crr[k]=brr[j];
+      k++;
+      j++;
+    }
+   }
+   //for entering the remaining elements inside the Array C
+   for(;i<length;i++)
+   {
+    crr[k]=arr[i];
+    k++;
+   }
+   for(;j<lengthb;j++)
+   {
+    crr[k]=brr[j];
+    k++;
+   }
+  lengthc = length + lengthb;
+  printf("Merging of Array A and Array B Successfull!!!!\n");
 }
 
 //1D array operation functions end here
@@ -1358,8 +1488,9 @@ int main()
 {
 //variables related to 1D Array Operations
     int key; //choice for menue switch case
-  int O=0; // menue loop variable
-
+    int O=0; // menue loop variable
+    int choice15;//choice variable for menue of Binary operations on array
+    int Q=0;//do while loop variable for 
 //variables relayed to 2D Array operations
 int a[10][10];
     int b[10][10];
@@ -1441,6 +1572,54 @@ printf("\n");
     printf("\n");
     switch(key)
     {
+
+      case 0:
+      printf("---------------BINARY OPERATIONS ON 1D ARRAY-----------------\n");
+      do
+      {
+      printf("Press 1 to Create Array A and Array B\n");
+      printf("Press 2 to Display array A; B & C \n");
+      printf("Press 3 to Merge Array A and Array B in Array C in ascending order\n");
+      printf("Press 4 to Go to the main Array menue\n");
+      scanf("%d",&choice15);
+      switch(choice15)
+      {
+        case 1:
+        CreateArrayTwo();
+        printf("\n");
+        break;
+
+        case 2:
+        displayArrayTwo();
+        printf("\n");
+        break;
+
+        case 3:
+        MergeArray();
+        printf("\n");
+        break;
+
+        case 4:
+        printf("------------------------Developer info----------------------\n");
+        printf("Name:- Aditya Kumar\n");
+        printf("Course:-B.Tech CS\n");
+        printf("Year:-2nd tear\n");
+        printf("Roll Number:-1901230100001\n");
+        printf("college code:-123\n");
+        printf("\n");
+        printf("Program terminated by the user........\n");
+        Q=4000;
+        printf("\n");
+        break;
+
+        default:
+        printf("Wrong choice!!!!...\n");
+        break;
+        }
+        }while(Q<3000);
+      printf("\n");
+      break;
+
       case 1:
       createArray(); //create array
       printf("\n");
