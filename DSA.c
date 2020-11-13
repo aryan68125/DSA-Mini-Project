8bda8bf5433edc7bfef40fbd12569674e9eb8fa3
Aditya Kumar <aryan68125@gmail.com>
2020-11-13 12:23:02 +0530

    Add files via upload

--- DSA.c
+++ DSA.c
@@ -1833,6 +1833,206 @@ void reverseDoublyLL()
 
 //......................Doubly Linked List ends here........................
 
+//......................Stack using linked list.............................
+struct node5
+{
+        int info5;
+        struct node5 *next5;
+};
+
+struct node5 *start5=NULL;
+
+void PushStackLL()
+{
+        struct node5 *temp5,*ptr5;
+        temp5=(struct node5 *)malloc(sizeof(struct node5));
+        if(temp5==NULL)
+        {
+                printf("\nStack overflow!!!!!.....\n");
+                exit(0);
+        }
+        printf("\nPush an element inside the stack....\n");
+        scanf("%d",&temp5->info5);
+        temp5->next5=NULL;
+        if(start5==NULL)
+        {
+                start5=temp5;
+        }
+        else
+        {
+                temp5->next5=start5;
+                start5=temp5;
+        }
+}
+
+void displayStackLL()
+{
+        struct node5 *ptr5;
+        if(start5==NULL)
+        {
+                printf("\nStack underflow!!!!!.....\n");
+                return;
+        }
+        else
+        {
+                ptr5=start5;
+                printf("\nElements in the stack is :-\n");
+                while(ptr5!=NULL)
+                {
+                        printf("%d\n",ptr5->info5 );
+                        ptr5=ptr5->next5 ;
+                }
+        }
+}
+
+void popStackLL()
+{
+  struct node5 *ptr5;
+        if(start5==NULL)
+        {
+                printf("\nStack underflow!!!!.....\n");
+                return;
+        }
+        else
+        {
+                ptr5=start5;
+                start5=start5->next5 ;
+                printf("\nPopping elements from the stack!!!!!!.....%d\n",ptr5->info5);
+                free(ptr5);
+        }
+}
+//...............to find wheter an equation is balanced or not using stack................
+
+struct node10
+{
+        char info10;
+        struct node10 *next10;
+};
+
+struct node10 *start10=NULL;
+
+void CreateExpressionLL()
+{
+        struct node10 *temp10,*ptr10;
+        temp10=(struct node10 *)malloc(sizeof(struct node10));
+        if(temp10==NULL)
+        {
+                printf("\nOut of memory!!!...\n");
+                exit(0);
+        }
+        printf("\nEnter the expression elements....\n");
+        scanf("%s",&temp10->info10);
+        temp10->next10=NULL;
+        if(start10==NULL)
+        {
+                start10=temp10;
+        }
+        else
+        {
+                ptr10=start10;
+                while(ptr10->next10!=NULL)
+                {
+                  ptr10=ptr10->next10;
+                }
+                ptr10->next10=temp10;
+                ptr10=temp10;
+        }
+}
+
+void displayExpressionLL()
+{
+        struct node10 *ptr10;
+        if(start10==NULL)
+        {
+                printf("\nNo expression found!!!!.....\n");
+                return;
+        }
+        else
+        {
+                ptr10=start10;
+                printf("\nExpression in linked list is :-\n");
+                while(ptr10!=NULL)
+                {
+                        printf("%c\t",ptr10->info10);
+                        ptr10=ptr10->next10 ;
+                }
+                printf("\n");
+        }
+}
+
+struct node11
+{
+   char info11;
+   struct node11 *next11;
+};
+
+struct node11 *start11=NULL;
+
+int isBalanced()
+{
+  int TF; //boolean variable
+   struct node10 *ptr10;
+   struct node11 *temp11;
+   struct node11 *ptr11;
+   temp11=(struct node11 *)malloc(sizeof(struct node11));
+   temp11->next11=NULL;
+        if(start10==NULL)
+        {
+                printf("\nNo expression found!!!!.....\n");
+        }
+        else
+        {
+                ptr10=start10;
+                
+                while(ptr10!=NULL)
+                {
+                        if(ptr10->info10 == '(' || ptr10->info10 == '[' || ptr10->info10 == '{')
+                        {          
+                          temp11->info11=ptr10->info10;  //insert at beginning in Linked List
+                                   if(start11==NULL)
+                                  {
+                                       start11=temp11;
+                                  }
+                                  else
+                                  {
+                                      temp11->next11=start11;
+                                      start11=temp11;
+                                  }
+                        }
+                        if(ptr10->info10 == ')' || ptr10->info10 == ']' || ptr10->info10 == '}')
+                        {
+                          
+                                if(start11==NULL)
+                                  {
+                                   TF=0;
+                                  }
+                                 else
+                                 {
+                                    ptr11=start11;
+                                    start11=start11->next11 ;
+                                    
+                                    free(ptr11);
+                                 }
+                        }
+                                    ptr10=ptr10->next10 ;
+                }
+
+                if(start11==NULL)
+                {
+                  TF=1;
+                }
+                else
+                {
+                  TF=0;
+                }
+                
+        }
+        return TF;
+}
+
+//..............STACK USING LINKED LIST ENDS HERE..............................
+
+
 int main()
 {
 //variables related to 1D Array Operations
@@ -1877,6 +2077,14 @@ int a[10][10];
     int c2=0; //to store the number of nodes in a doubly linked list
     int choice3; //menue choice variable for doubly linked list menue
 
+    //menue choice of stack menue
+    int choice5;
+    int exit=0;
+    //submenue variabls of main stack menue program
+    int exit10=0;
+    int Balanced;
+    int choice10;
+
 //main menue loop
 do{
     
@@ -1885,7 +2093,8 @@ printf("Press 1 to perform 1D Array operations\n");
 printf("Press 2 to perform 2D Array operations\n");
 printf("Press 3 to perform Singly Linked List operations\n");
 printf("Press 4 to perform Doubly Linked List operations\n");
-printf("Press 5 to exit program\n");
+printf("Press 5 to perform stack operations\n");
+printf("Press 6 to exit program\n");
 scanf("%d", &menue);
 printf("\n");
 switch(menue)
@@ -1899,7 +2108,7 @@ printf("\n");
   
   do
   {
-    printf("---------------------------Menue--------------------------------\n");
+    printf("---------------------------1D Array Menue--------------------------------\n");
     printf("Press 0 to do Binary Operations on the Array\n");
     printf("Press 1 to Create the Array\n");
     printf("Press 2 to Display the Array\n");
@@ -2086,7 +2295,7 @@ printf("\n");
 //menue loop for 2D Array
  do
     {
-    printf("\n               MENUE                \n");
+    printf("\n               2D Array MENUE                \n");
     printf("Press 1 to enter matrix a\n");
     printf("Press 2 to enter matrix b\n");
     printf("Press 3 to display matrix a\n");
@@ -2213,7 +2422,7 @@ printf("\ninitializing Singly Linked List operations program\n");
 printf("\n");
         do{
                // menue of Singly Linked List Operation
-                printf("\n                MENU                             \n");
+                printf("\n                Singly Linked List MENU                             \n");
                 printf("\n 1.Create     \n");
                 printf("\n 2.Display    \n");
                 printf("\n 3.Insert at the beginning    \n");
@@ -2330,7 +2539,7 @@ printf("\ninitializing Doubly Linked List operations program\n");
 printf("\n");
 do
 {
-printf("-----------------------MENUE----------------------\n");
+printf("-----------------------Doubly Linked List MENUE----------------------\n");
 printf("Press 1 to create a node in Doubly Linked List\n");
 printf("Press 2 to display all the nodes of Doubly Linked List\n");
 printf("Press 3 to Insert the newly created node at the beginning of the Doubly Linked list\n");
@@ -2431,6 +2640,101 @@ break;
 break;
 
 case 5:
+  do{
+               
+                printf("\n                STACK MENU                             \n");
+                printf("\n 1.Push the element in stack   \n");
+                printf("\n 2.Display elements in stack    \n");
+                printf("\n 3.Pop the element in stack   \n");
+                printf("\n 4.check whether the equation is balanced or not using stack \n");
+                printf("\n 5.Exit       \n");
+                printf("\n--------------------------------------\n");
+                printf("\nEnter your choice:\n");
+                scanf("%d",&choice5);
+                switch(choice5)
+                {
+                        case 1:
+                                        PushStackLL();
+                                        break;
+                        case 2:
+                                        displayStackLL();
+                                        break; 
+                        case 3:
+                                        popStackLL();
+                                        break;   
+                        case 4:
+                                        do{
+               
+                printf("\n                MENU                             \n");
+                printf("\n 1.Create a linked list and store elements of an expression inside the linked list   \n");
+                printf("\n 2.Display the elements of the expression stored inside the linked list    \n");
+                printf("\n 3.To check whether th equation is balanced or not   \n");
+                printf("\n 4.Exit       \n");
+                printf("\n--------------------------------------\n");
+                printf("\nEnter your choice:\n");
+                scanf("%d",&choice10);
+                switch(choice10)
+                {
+                        case 1:
+                                        CreateExpressionLL();
+                                        break;
+                        case 2:
+                                        displayExpressionLL();
+                                        break; 
+                        case 3:
+                                        Balanced=isBalanced();
+                                        if(Balanced==1)
+                                        {
+                                          printf("The equation is Balanced.....\n");
+                                        }
+                                        else
+                                        {
+                                          printf("The equation is not balanced\n");
+                                        }
+                                        printf("\n");
+                                        break;    
+                        
+                        case 4:
+                                        printf("This piece of program was written by\n");
+                                        printf("Name : Aditya Kumar\n");
+                                        printf("Roll number : 1901230100001\n");
+                                        printf("College code: 123\n");
+                                        printf("College Name : SITM\n");
+                                        printf("Course: B.Tech\n");
+                                        printf("Branch : Computer science\n");
+                                        printf("Year : 2nd Year\n");
+                                        printf("Thank you for using my program ....Bye\n");
+                                        exit10=5000;
+                                        break;
+                             
+                        default:
+                                        printf("\n Wrong Choice:\n");
+                                        break;
+                }
+            }while(exit10<4000);
+                                        break; 
+                        
+                        case 5:
+                                        printf("This piece of program was written by\n");
+                                        printf("Name : Aditya Kumar\n");
+                                        printf("Roll number : 1901230100001\n");
+                                        printf("College code: 123\n");
+                                        printf("College Name : SITM\n");
+                                        printf("Course: B.Tech\n");
+                                        printf("Branch : Computer science\n");
+                                        printf("Year : 2nd Year\n");
+                                        printf("Thank you for using my program ....Bye\n");
+                                        exit = 7000;
+                                        break;
+                             
+                        default:
+                                        printf("\n Wrong Choice:\n");
+                                        break;
+                }
+        }while(exit<6000);
+break;
+
+case 6:
 printf("This piece of program was written by\n");
 printf("Name : Aditya Kumar\n");
 printf("Roll number : 1901230100001\n");
