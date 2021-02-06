2ad071b2947c17c2e28b107bb9e81abcb82d474b
Aditya Kumar <aryan68125@gmail.com>
2021-02-06 18:57:49 +0530

    Add files via upload

--- DSA.c
+++ DSA.c
@@ -2041,6 +2041,7 @@ struct QueueLL // here struct QueueLL =  struct node
 	struct QueueLL *nextQueue; // struct QueueLL *nextQueue; = struct node *next;
 };
 struct QueueLL *startQueue=NULL; //struct QueueLL *startQueue = struct node *start;
+struct QueueLL *tailQueue=NULL;   
 
 /*
 this function is used to enter elements inside the queue
@@ -2058,16 +2059,12 @@ void enqueueLL()
     temp->nextQueue=NULL;
     if(startQueue==NULL)
     {
-    	startQueue=temp;
+    	startQueue=tailQueue=temp;
     }
     else
     {
-    	ptr = startQueue;
-    	while(ptr->nextQueue!=NULL)
-    	{
-    		ptr=ptr->nextQueue;
-    	}
-    	ptr->nextQueue=temp;
+        tailQueue->nextQueue = temp;
+    	tailQueue=temp; //this will set the address of the last node to the tail pointer in the linked list
     }
 }
 
